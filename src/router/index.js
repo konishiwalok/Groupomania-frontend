706c6c4143667fb77977ecf5aebf3954a653a197
sophie <sophie-girard91@outlook.fr>
2021-09-09 09:40:22 +0200

    home

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,12 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
+const routes = [{
+    path: '/',
+    name: 'Home',
+    component: () =>
+        import ('../views/HomeView.vue')
+},
     {
         path: '/signup',
         name: 'Signup',
--- src/views/AllPostView.vue
+++ src/views/AllPostView.vue
@@ -1,5 +1,5 @@
 <!-- LA PAGE D'ACCUEIL  + importation Post.vue = slot = LE POST -COMMENTS -NEW COMMENT + jumbo + btnPost-->
-
+<template>
   <div class="min-vh-100 d-flex flex-column justify-content-between ">
     <NavbarPost />
     <Jumbo />
