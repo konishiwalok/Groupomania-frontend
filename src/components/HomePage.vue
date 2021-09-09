9f19aaec7765b38379f53b40afc11c06ee83f501
sophie <sophie-girard91@outlook.fr>
2021-09-09 13:12:17 +0200

    home.

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container-fluid text-white">
         <div>
-            <h1 class="my-3">Bienvenue sur Intramania</h1>
-            <p class="h6 py-2"><em>La plateforme communautaire de Groupomania</em></p>
+            <h1 class="my-3">Bienvenue sur Groupoamania</h1>
+
             <hr class="mt-4 line">
         </div>
     </div>
